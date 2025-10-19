1f278a2178ad28cea6dcc343728c7b1e147fb6a4
KonuhovAND <andreykonuhov8@gmail.com>
2025-10-19 13:47:57 +0300

    Refactor token handling in ask_your_token.py and update bot.py to improve command handling and logging

--- keys/tools/ask_your_token.py
+++ keys/tools/ask_your_token.py
@@ -2,7 +2,7 @@ import os
 
 
 def ask_your_token():
-    token= input("Enter your telegram bot token: ")
-    with open('./keys/_token.py','w') as file:
-        file.write(f"_token= '{token}'\n") 
-# ask_your_token()
+    if not os.path.exists('./keys/_token.py'):
+        token= input("Enter your telegram bot token: ")
+        with open('./keys/_token.py','w') as file:
+            file.write(f"_token= '{token}'\n") 
--- tools/bot.py
+++ tools/bot.py
@@ -1,17 +1,13 @@
 import asyncio
-import sqlite3
-from tools.jsonhandler import log
+import logging as log
+import os
+import signal
+import warnings
+from telegram.warnings import PTBUserWarning
 from text_data.sm import dev_profile, scenario
-from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-    MessageHandler,
-    Updater,
-    filters,
-)
-import time
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler, CallbackQueryHandler
+from telegram.error import TelegramError
 
 
 class tg_bot:
@@ -22,10 +18,14 @@ class tg_bot:
         self.token_bot = token_bot
         self.application = None
 
+    # async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #     log.info("user want to see start menu")
+    #     await update.message.reply_text(dev_profile)
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        log.info("user want to see start menu")
-        await update.message.reply_text(dev_profile)
-
+        keyboard = [['Button 1', 'Button 2'], ['Button 3']]
+        reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        await update.message.reply_text('Choose:', reply_markup=reply_markup)
+        
     async def options(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         option = update.message.text.strip()[1:]
         log.info(scenario[option]["log"])
@@ -72,7 +72,7 @@ class tg_bot:
     def main(self):
         self.application = Application.builder().token(self.token_bot).build()
         log.info("resive message")
-        self.application.add_handler(CommandHandler("start", self.start))
+        self.application.add_handler(ConversationHandler(entry_points=[CommandHandler("start", self.start)],states={},fallbacks=[]))
         self.application.post_init = self.post_init
         self.application.add_handler(CommandHandler("show_logs", self.show_log))
         self.application.add_handler(
